8d87a0984acf84c1d3209a6eef08df368662d5fb
JustinQuanCC <justin.quan@mycc.caspercollege.edu>
2023-10-21 10:39:27 -0600

    attempted analysis

--- code.js
+++ code.js
@@ -1,10 +1,11 @@
 function convertToAdjMatrix(adjList) {
     var v = adjList.length
     var adjMat = Array.from(Array(v), ()=>Array(v).fill(0)) // from here: https://www.geeksforgeeks.org/convert-adjacency-list-to-adjacency-matrix-representation-of-a-graph/
-
+    //I have no idea what the runtime is for creating a 2d array, assuming its v^2 as it needs to create rows and columns
     for(var k = 0; k < v; k++){
         for (var j of adjList[k]){
             adjMat[k][j] = 1
+            // console.log(adjMat) used to check how many times it loops in order to swap a number. It takes E amount of loops
         }
     }
     return adjMat;
@@ -30,4 +31,4 @@ function convertToAdjList(adjMat) {
     return adjList;
 }
 
-console.log(convertToAdjList([[ 0, 1, 0, 0, 1 ], [ 1, 0, 1, 1, 1 ], [ 0, 1, 0, 1, 0 ], [ 0, 1, 1, 0, 1 ], [ 1, 1, 0, 1, 0 ]])) // debugging
+//console.log(convertToAdjList([[ 0, 1, 0, 0, 1 ], [ 1, 0, 1, 1, 1 ], [ 0, 1, 0, 1, 0 ], [ 0, 1, 1, 0, 1 ], [ 1, 1, 0, 1, 0 ]])) // debugging
